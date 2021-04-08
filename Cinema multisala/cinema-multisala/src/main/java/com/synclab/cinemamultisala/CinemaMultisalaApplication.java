8233a4ee323fe57e282b9b1cb1e006ff75ceec52
Daniele Scalco <danielescalco@hotmail.it>
2021-04-08 09:22:47 +0200

    Creazione base progetto maven

---
+++ Cinema multisala/cinema-multisala/src/main/java/com/synclab/cinemamultisala/CinemaMultisalaApplication.java
@@ -0,0 +1,13 @@
+package com.synclab.cinemamultisala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaMultisalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaMultisalaApplication.class, args);
+	}
+
+}
---
+++ Cinema multisala/cinema-multisala/src/test/java/com/synclab/cinemamultisala/CinemaMultisalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synclab.cinemamultisala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaMultisalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
